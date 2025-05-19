f182ef39917751d380cdd79341ffd6e918976d01
Anh <nguay089@mymail.unisa.edu.au>
2025-05-19 21:25:28 +0930

    Add project files.

---
+++ Project 1/Controllers/ABoutController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project_1.Models;
+
+namespace Project_1.Controllers
+{
+    public class ABoutController : Controller
+    {
+        private static List<StoreViewModel> stores = new List<StoreViewModel>();
+        public IActionResult Index()
+        {
+           return View(stores);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateDrink(StoreViewModel storeViewModel)
+        {
+            //return View("Index");
+            stores.Add(storeViewModel); 
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "About us";
+        }
+    }
+}
---
+++ Project 1/Models/StoreViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_1.Models
+{
+    public class StoreViewModel
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+}
